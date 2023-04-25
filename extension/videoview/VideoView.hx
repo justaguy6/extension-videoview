93b8498fb063598b8857b1da84b23ef505c6d960
Random people <98900217+justaguy6@users.noreply.github.com>
2023-04-25 07:15:41 -0300

    Update VideoView.hx

--- extension/videoview/VideoView.hx
+++ extension/videoview/VideoView.hx
@@ -4,41 +4,35 @@ package extension.videoview;
 import lime.system.JNI;
 #end
 
-/**
- * author: Saw (M.A. Jigsaw)
- */
-	
-class VideoView
-{
-	public static var onCompletion:Void -> Void = null;
-	public static var onPrepared:Void -> Void = null;
+class VideoView {
+    public static var onCompletion:Void->Void = null;
+    public static var onPrepared:Void->Void = null;
 
-	public static function playVideo(path:String = null):Void 
-	{
-		#if android
-		var _callbackFunc = JNI.createStaticMethod("extensions/videoview/VideoViewExtension", "setCallback", "(Lorg/haxe/lime/HaxeObject;)V");
-		var _playVideo = JNI.createStaticMethod("extensions/videoview/VideoViewExtension", "playVideo", "(Ljava/lang/String;)V");
+    private static var callback:CallBack;
 
-		_callbackFunc(new CallBack());
-		_playVideo(path);
-		#end
-	}
+    public static function playVideo(path:String = null):Void {
+        #if android
+        if (callback == null) {
+            callback = new CallBack();
+            JNI.createStaticMethod("extensions/videoview/VideoViewExtension", "setCallback", "(Lorg/haxe/lime/HaxeObject;)V")(callback);
+        }
+        JNI.createStaticMethod("extensions/videoview/VideoViewExtension", "playVideo", "(Ljava/lang/String;)V")(path);
+        #end
+    }
 }
 
 class CallBack {
-	public function new() {
+    public function new() {}
 
-	}
+    public function onCompletion() {
+        if (VideoView.onCompletion != null) {
+            VideoView.onCompletion();
+        }
+    }
 
-	public function onCompletion() {
-		if (VideoView.onCompletion != null) {
-			VideoView.onCompletion();
-		}
-	}
-
-	public function onPrepared() {
-		if (VideoView.onPrepared != null) {
-			VideoView.onPrepared();
-		}
-	}
+    public function onPrepared() {
+        if (VideoView.onPrepared != null) {
+            VideoView.onPrepared();
+        }
+    }
 }
